369a39f65c2843edf69004c41e765db5859affc0
deko possas <andrepossaster@gmail.com>
2021-05-06 16:48:14 -0300

    state com this state

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Player from './components/Player';
 import Sidebar from './components/Sidbar';
+import categories from './data';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [...categories]
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <Player />
-        <Sidebar />
+        <Sidebar categories={this.state.categories} />
       </div>
     );
   }
