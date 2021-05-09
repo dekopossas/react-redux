9b93c3a13ecc61cd715e08194aec646ead8807fc
deko possas <andrepossaster@gmail.com>
2021-05-09 16:08:26 -0300

    dispacth funcionando

--- reactredux/src/components/Player.js
+++ reactredux/src/components/Player.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Player extends React.Component {
   render() {
+    const { selectedMovie, selectedCategory } = this.props
     return (
       <div>
-        <h1>Categoria 1</h1>
-        <h2>Filme 1</h2>
+        <h1>Categoria {selectedCategory.name}</h1>
+        <h2>Filme {selectedMovie.title}</h2>
       </div>
     )
   }
-}
+};
 
-export default Player;
+const mapStateToProps = (state) => ({
+  selectedCategory: state.movieReducer.selectedCategory,
+  selectedMovie: state.movieReducer.selectedMovie,
+});
+
+export default connect(mapStateToProps) (Player) ;
