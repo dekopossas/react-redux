e5ac034b3beed9e1fea167953949c95df8a02aa8
deko possas <andrepossaster@gmail.com>
2021-05-11 22:49:54 -0300

    rederizando os componentes com provider

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Player from './components/Player';
+import SideBar from './components/SideBar';
+import categories from './data';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [...categories],
+    }
+  }
 
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
+      <div>
+        <Provider store={store}>
+          <Player />
+          <SideBar />
+        </Provider>
       </div>
     );
   }
