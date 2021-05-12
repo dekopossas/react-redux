98dcb2a740e64c0d446f46caf3d0fd1ea43c4757
deko possas <andrepossaster@gmail.com>
2021-05-12 14:12:34 -0300

    melhor aula de redux que ja existiu na historia

---
+++ reactredux/src/componets/Player.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Player extends Component {
+  render() {
+    const { category, movie } = this.props;
+    return (
+      <div>
+        <h1>Category: {category.name}</h1>
+        <h2>Movie: {movie.title}</h2>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  category: state.movieReducer.selectedCategory,
+  movie: state.movieReducer.selectedMovie,
+})
+
+export default connect(mapStateToProps)(Player);
